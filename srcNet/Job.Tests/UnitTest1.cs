f2ac78dc98b806f5100b1b21d5cc9da8119b5374
Kaiwanyawit Chawankul <kaiwanyawit.chawankul@dominos.com.au>
2023-03-29 13:58:19 +0700

    Add Class

--- srcNet/Job.Tests/UnitTest1.cs
+++ srcNet/Job.Tests/UnitTest1.cs
@@ -1,11 +1,23 @@
+using FluentAssertions;
+
 namespace Job.Tests;
+
+public class Sample
+{
+    public int Plus(int A,int B)
+    {
+        return A+B;
+    }
+}
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        var sut = new Sample();
+        var result = sut.Plus(1,1);
+        result.Should().Be(2);
     }
 
     [Fact]
